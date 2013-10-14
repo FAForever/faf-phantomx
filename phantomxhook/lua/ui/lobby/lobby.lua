2968d455eadf7ef68d819cb0a088b03855f384a7
Duck_42 <devnull@localhost>
2013-10-14 11:44:32 -0500

    Added CPU benchmark testing code.  Test code can co-exist with current benchmark to provide relative reference.

--- phantomxhook/lua/ui/lobby/lobby.lua
+++ phantomxhook/lua/ui/lobby/lobby.lua
@@ -61,3 +61,229 @@ function AssignRandomFactions(gameInfo)
 	--Note tha by this point, any "Random - No UEF" players should have a faction and so, should not be assigned another
 	PX_oldAssignRandomFactions(gameInfo)
 end
+
+
+--******************************************************************************************************
+-- CPU Benchmark Test Code
+-- Author: Duck_42
+-- Date: 2013.04.05
+-- 
+-- 2013.10.14 - Testing new CPU benchmark code.
+--******************************************************************************************************
+
+--CPU Status Bar Configuration
+local t2_barMax = 450
+local t2_barMin = 150
+local t2_greenBarMax = 300
+local t2_yellowBarMax = 375
+local t2_scoreSkew1 = 0 --Skews all CPU scores up or down by the amount specified (0 = no skew)
+local t2_scoreSkew2 = 1.0 --Skews all CPU scores specified coefficient (1.0 = no skew)
+
+--Variables for CPU Test
+local t2_benchTime
+local t2_firstCPUTest = true
+
+--------------------------------------------------
+--  CPU Benchmarking Functions
+--------------------------------------------------
+function t2_CPUBenchmark()
+    --This function gives the CPU some busy work to do.
+    --CPU score is determined by how quickly the work is completed.
+    local totalTime = 0
+    local lastTime
+    local currTime
+    
+    --Make everything a local variable
+    --This is necessary because we don't want LUA searching through the globals as part of the benchmark
+    local h
+    local i
+    local j
+    local k
+    local l
+    local m
+    for h = 1, 225, 1 do 
+        lastTime = GetSystemTimeSeconds()
+        for i = 1.0, 4.0, 0.000008 do 
+            j = i + i
+            k = i * i
+            l = k / j
+            m = j - i
+        end
+        currTime = GetSystemTimeSeconds()
+        totalTime = totalTime + currTime - lastTime
+
+        --This is necessary in order to make this 'thread' yield so other things can be done.
+        WaitSeconds(0)
+    end
+
+    t2_benchTime = math.ceil(totalTime * 100)
+end
+
+--------------------------------------------------
+--  CPU GUI Functions
+--------------------------------------------------
+function CreateCPUMetricUI()
+    --This function handles creation of the CPU benchmark UI elements (statusbars, buttons, tooltips, etc)
+    local StatusBar = import('/lua/maui/statusbar.lua').StatusBar
+	
+    if not singlePlayer then
+        for i= 1, LobbyComm.maxPlayerSlots do
+                GUI.slots[i].CPUSpeedBar = StatusBar(GUI.slots[i].pingGroup, barMin, barMax, false, false,
+                    UIUtil.SkinnableFile('/game/unit_bmp/bar-back_bmp.dds'),
+                    UIUtil.SkinnableFile('/game/unit_bmp/bar02_bmp.dds'),
+                    true)
+                LayoutHelpers.AtBottomIn(GUI.slots[i].CPUSpeedBar, GUI.slots[i].pingGroup, 1)
+                LayoutHelpers.AtLeftIn(GUI.slots[i].CPUSpeedBar, GUI.slots[i].pingGroup, 0)
+                LayoutHelpers.AtRightIn(GUI.slots[i].CPUSpeedBar, GUI.slots[i].pingGroup, 0)
+                CPU_AddControlTooltip(GUI.slots[i].CPUSpeedBar, 0, i)
+                GUI.slots[i].CPUSpeedBar.CPUActualValue = 450
+                
+				GUI.slots[i].CPUSpeedBar_t2 = StatusBar(GUI.slots[i].pingGroup, barMin, barMax, false, false,
+                    UIUtil.SkinnableFile('/game/unit_bmp/bar-back_bmp.dds'),
+                    UIUtil.SkinnableFile('/game/unit_bmp/bar-01_bmp.dds'),
+                    true)
+				LayoutHelpers.AtBottomIn(GUI.slots[i].CPUSpeedBar_t2, GUI.slots[i].pingGroup, 5)
+                LayoutHelpers.AtLeftIn(GUI.slots[i].CPUSpeedBar_t2, GUI.slots[i].pingGroup, 0)
+                LayoutHelpers.AtRightIn(GUI.slots[i].CPUSpeedBar_t2, GUI.slots[i].pingGroup, 0)
+                t2_CPU_AddControlTooltip(GUI.slots[i].CPUSpeedBar_t2, 0, i)
+                GUI.slots[i].CPUSpeedBar_t2.CPUActualValue = 450
+        end
+
+        GUI.rerunBenchmark.OnClick = function(self, modifiers)
+            GUI.rerunBenchmark:Disable()
+            ForkThread(function() t2_StressCPU(1) end)
+        end
+    end
+end
+
+function t2_CPU_AddControlTooltip(control, delay, slotNumber)
+    --This function creates the benchmark tooltip for a slot along with necessary mouseover function.
+    --It is called during the UI creation.
+    --    control: The control to which the tooltip is to be added.
+    --    delay: Amount of time to delay before showing tooltip.  See Tooltip.CreateMouseoverDisplay for info.
+    --  slotNumber: The slot number associated with the control.
+    if not control.oldHandleEvent then
+        control.oldHandleEvent = control.HandleEvent
+    end
+    control.HandleEvent = function(self, event)
+        if event.Type == 'MouseEnter' then
+            local slot = slotNumber
+            Tooltip.CreateMouseoverDisplay(self, {text='CPU Rating #2: '..GUI.slots[slot].CPUSpeedBar_t2.CPUActualValue_t2,
+                                           body='0=Fastest, 450=Slowest'}, delay, true)
+        elseif event.Type == 'MouseExit' then
+            Tooltip.DestroyMouseoverDisplay()
+        end
+        return self.oldHandleEvent(self, event)
+    end
+end
+
+function t2_StressCPU(waitTime)
+    --This function instructs the PC to do a CPU score benchmark.
+    --It handles the necessary UI updates during the benchmark, sends
+    --the benchmark result to other players when finished, and it updates the local
+    --user's UI with their new result.
+    --    waitTime: The delay in seconds that this function should wait before starting the benchmark.
+
+    for i = waitTime, 1, -1 do
+        GUI.rerunBenchmark.label:SetText(i..'s')
+        WaitSeconds(1)
+    end
+
+    --Get our last benchmark (if there was one)
+    local currentBestBenchmark = FindBenchmarkForName(localPlayerName..'_t2')
+    if currentBestBenchmark == false then
+        currentBestBenchmark = 10000
+    end
+
+    --LOG('Beginning CPU benchmark')
+    GUI.rerunBenchmark.label:SetText('. . .')
+
+    --Run three benchmarks and keep the best one
+    for i=1, 3, 1 do
+        t2_benchTime = 0
+
+        t2_CPUBenchmark()
+
+        t2_benchTime = t2_scoreSkew2 * t2_benchTime + t2_scoreSkew1
+
+        --LOG('CPU benchmark #'..i..' complete: '.. benchTime )
+
+        --If this benchmark was better than our best so far...
+        if t2_benchTime < currentBestBenchmark then
+            --Make this our best benchmark
+            currentBestBenchmark = t2_benchTime
+
+            --Send it to the other players
+            lobbyComm:BroadcastData( { Type = 'CPUBenchmark', PlayerName = localPlayerName..'_t2', Result = currentBestBenchmark} )
+
+            --Add the benchmark to the local benchmark table
+            AddPlayerBenchmark({PlayerName = localPlayerName..'_t2', Result = currentBestBenchmark})
+
+            --Update the UI bar
+            UpdateCPUBar(localPlayerName)
+        end
+    end
+
+    --Set this flag so we'll know later
+    t2_firstCPUTest = false
+
+	orig_StressCPU(1)
+end
+
+orig_StressCPU = StressCPU
+
+function StressCPU(waitTime)
+	t2_StressCPU(waitTime)
+end
+
+function SetSlotCPUBar(slot, playerInfo)
+    --This function updates the UI with a CPU benchmark bar for the specified slot/playerInfo.
+    --    slot: a numbered slot (1-however many slots there are for this map)
+    --    playerInfo: The corresponding playerInfo object from gameInfo.PlayerOptions[slot].
+
+    if GUI.slots[slot].CPUSpeedBar then
+        GUI.slots[slot].CPUSpeedBar:Hide()
+        if playerInfo.Human then
+            local b = FindBenchmarkForName(playerInfo.PlayerName)
+            if b then
+                -- For display purposes, the bar has a higher minimum that the actual barMin value.
+                -- This is to ensure that the bar is visible for very small values
+                local clampedResult =  math.max(math.min(b.Result, barMax), barMin + math.floor(.04 * (barMax - barMin)))
+                GUI.slots[slot].CPUSpeedBar:SetValue(clampedResult)
+                
+                --For the tooltip, we use the actual clamped value
+                GUI.slots[slot].CPUSpeedBar.CPUActualValue = b.Result
+                GUI.slots[slot].CPUSpeedBar:Show()
+
+                GUI.slots[slot].CPUSpeedBar._bar:SetTexture(UIUtil.SkinnableFile('/game/unit_bmp/bar02_bmp.dds'))
+
+                -- if clampedResult <= greenBarMax then
+                    -- GUI.slots[slot].CPUSpeedBar._bar:SetTexture(UIUtil.SkinnableFile('/game/unit_bmp/bar-02_bmp.dds'))
+                -- elseif clampedResult <= yellowBarMax then
+                    -- GUI.slots[slot].CPUSpeedBar._bar:SetTexture(UIUtil.SkinnableFile('/game/unit_bmp/bar-01_bmp.dds'))
+                -- else
+                    -- GUI.slots[slot].CPUSpeedBar._bar:SetTexture(UIUtil.SkinnableFile('/game/unit_bmp/bar-03_bmp.dds'))
+                -- end
+            end
+        end
+    end
+	
+	if GUI.slots[slot].CPUSpeedBar_t2 then
+        GUI.slots[slot].CPUSpeedBar_t2:Hide()
+        if playerInfo.Human then
+            local b = FindBenchmarkForName(playerInfo.PlayerName..'_t2')
+            if b then
+                -- For display purposes, the bar has a higher minimum that the actual barMin value.
+                -- This is to ensure that the bar is visible for very small values
+                local clampedResult =  math.max(math.min(b.Result, barMax), barMin + math.floor(.04 * (barMax - barMin)))
+                GUI.slots[slot].CPUSpeedBar_t2:SetValue(clampedResult)
+                
+                --For the tooltip, we use the actual clamped value
+                GUI.slots[slot].CPUSpeedBar_t2.CPUActualValue_t2 = b.Result
+                GUI.slots[slot].CPUSpeedBar_t2:Show()
+
+                GUI.slots[slot].CPUSpeedBar_t2._bar:SetTexture(UIUtil.SkinnableFile('/game/unit_bmp/bar-01_bmp.dds'))
+            end
+        end
+    end
+end
