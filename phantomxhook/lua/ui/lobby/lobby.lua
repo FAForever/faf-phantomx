58636f0c687fe9471067a5e8d36469ff5ab5bfaf
Duck_42 <devnull@localhost>
2015-04-11 13:55:28 -0500

    Added faction selector icons for use by the new lobby code. Modified lobby hooks to support Random but not UEF option.

--- phantomxhook/lua/ui/lobby/lobby.lua
+++ phantomxhook/lua/ui/lobby/lobby.lua
@@ -38,71 +38,56 @@ function MapHasCivilianArmy(scenario)
 end
 
 --Random, but not UEF faction option
-table.insert(factionBmps, "/faction_icon-sm/random_ico_no_uef.dds")
-table.insert(factionTooltips, 'lob_random_no_uef')
-local randomFactionID_noUEF = table.getn(FactionData.Factions) + 2
+FACTION_NAMES[6] = "random"
 
-local PX_oldFaction_Selector_Set_Enabled = Faction_Selector_Set_Enabled
-function Faction_Selector_Set_Enabled(enabled, faction)
-	--If Random but no UEF, then use value for random to avoid problems	
-	if faction == randomFactionID_noUEF then
-		faction = randomFactionID_noUEF - 1
-	end
+local RandomNoUEFFaction = {
+        Key = 'randomNoUEF',
+        DisplayName = "Random, but not UEF",        
+        SmallIcon = "/faction_icon-sm/random_ico_no_uef.dds",
+        TooltipID = 'lob_random_no_uef',
+    }
 	
-	--Call the original function
-	PX_oldFaction_Selector_Set_Enabled(enabled, faction)
+table.insert(FactionData.Factions, RandomNoUEFFaction)
+
+local LobbyManager_Original_CreateSlotsUI = CreateSlotsUI
+
+
+local function GetRandomFactionIndex()
+    local randomfaction = nil
+    local counter = 50
+    while counter > 0 do
+        counter = (counter - 1)
+        randomfaction = math.random(1, table.getn(FactionData.Factions)-1)
+    end
+    return randomfaction
 end
-
-local PX_oldupdateFactionSelectorIcons = updateFactionSelectorIcons
-function updateFactionSelectorIcons(enabled, faction)
-	--If Random but no UEF, then use value for random to avoid problems
-	if faction == randomFactionID_noUEF then
-		faction = randomFactionID_noUEF - 1
-	end
-	
-	--Call the original function
-	PX_oldupdateFactionSelectorIcons(enabled, faction)
-end
-
-local PX_oldSetCurrentFactionTo_Faction_Selector = SetCurrentFactionTo_Faction_Selector
-function SetCurrentFactionTo_Faction_Selector(input_faction)
-	local faction = input_faction or Prefs.GetFromCurrentProfile('LastFaction') or 1
-	
-	--If Random but no UEF, then use value for random to avoid problems	
-	if faction == randomFactionID_noUEF then
-		LOG('Random but no UEF detected, using random')
-		faction = randomFactionID_noUEF - 1
-	end
-	
-	--Call the original function
-	PX_oldSetCurrentFactionTo_Faction_Selector(faction)
-end
-
 
 function GetRandomFactionIndex_noUEF()
     local randomfaction
+	local counter = 50
+	
     --Loop 50 times (see LUA documentation on math.random)
-    for i = 1, 50 do
-        --Exclude UEF from selection range
-        randomfaction = math.random(2, table.getn(FactionData.Factions))
+	while counter > 0 do
+		--Exclude UEF from selection range
+        counter = (counter - 1)
+        randomfaction = math.random(2, table.getn(FactionData.Factions)-1)
     end
     return randomfaction
 end
 
 local PX_oldAssignRandomFactions = AssignRandomFactions
-function AssignRandomFactions(gameInfo)	
+function AssignRandomFactions()
     --Handle the "Random - No UEF" option
-    local randomFactionID_noUEF = table.getn(FactionData.Factions) + 2
-    
+	local randomNoUEFFactionID = table.getn(FactionData.Factions)
     for index, player in gameInfo.PlayerOptions do
-        if player.Faction >= randomFactionID_noUEF then
+		if player.Faction == randomNoUEFFactionID then
             player.Faction = GetRandomFactionIndex_noUEF()
         end
     end
         
     --Handle the regular "Random" option
     --Note that by this point, any "Random - No UEF" players should have a faction and so, should not be assigned another
-    PX_oldAssignRandomFactions(gameInfo)
+    PX_oldAssignRandomFactions()
 end
 
 --Help UI Changes
@@ -115,8 +100,10 @@ end
 
 function CreatePhantomLobbyUI()
 	--This function handles creation of Phantom-X lobby UI elements (buttons, tooltips, etc)
-    GUI.showPhantomXHelp = UIUtil.CreateButtonStd(GUI.panel, '/BUTTON/small/', 'About Phantom-X', 9, 0)
-	LayoutHelpers.AtLeftTopIn(GUI.showPhantomXHelp, GUI.panel, GUI.panel.Width()-218, 320)
+	
+    GUI.showPhantomXHelp = UIUtil.CreateButtonWithDropshadow(GUI.panel, '/BUTTON/medium/', 'How To Play')
+	LayoutHelpers.AtLeftIn(GUI.showPhantomXHelp, GUI.chatPanel, 190)
+    LayoutHelpers.AtVerticalCenterIn(GUI.showPhantomXHelp, GUI.launchGameButton, -6)
 	
 	GUI.showPhantomXHelp.OnClick = function(self, modifiers)
 		import('/modules/help_ui.lua').ShowPhantomXHelpDialog(GUI.panel)
