9ee0da489e75327b5e89e12341121f95cec93fc6
IceDreamer <aeoncleanse@users.noreply.github.com>
2019-02-01 02:59:40 +0000

    Check mods for icons etc

--- phantomxhook/lua/ui/uiutil.lua
+++ phantomxhook/lua/ui/uiutil.lua
@@ -1,9 +1,9 @@
 local baseUIFile = UIFile
-function UIFile(filespec)
+function UIFile(filespec, checkMods)
 	local NukePingModPath = '/phantomxhook'
 	#Ignore skin paths for texture loads out of the mod file.
 	if string.find(filespec, NukePingModPath) == nil then
-		return baseUIFile(filespec)
+		return baseUIFile(filespec, checkMods)
 	else
 		return filespec
 	end
