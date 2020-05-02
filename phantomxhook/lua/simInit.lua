4939129285cdd450d7153fbdb91a426152650dbe
shalkya <PYL35@hotmail.fr>
2020-05-02 19:10:38 +0200

    compatibility with new scoreboard

--- phantomxhook/lua/simInit.lua
+++ phantomxhook/lua/simInit.lua
@@ -1,11 +1,11 @@
-#****************************************************************************
-#**
-#**  File     :  /hook/lua/simInit.lua
-#**  Author(s):  novaprim3
-#**
-#**  Summary  :  Multi-Phantom Mod for Forged Alliance
-#**
-#****************************************************************************
+--****************************************************************************
+--**
+--**  File     :  /hook/lua/simInit.lua
+--**  Author(s):  novaprim3
+--**
+--**  Summary  :  Multi-Phantom Mod for Forged Alliance
+--**
+--****************************************************************************
 local modPath = 'Phantom-X'
 
 function ShuffleStartPositions(syncNewPositions) -- Disable spawn shuffling and syncing since everyone starts allied
@@ -19,4 +19,5 @@ function BeginSession()
     if( tonumber(ScenarioInfo.Options.Phantom_Meteor)) == 1 then
         ForkThread(import('/modules/meteors.lua').MeteorsThread)
     end
+    import('/lua/sim/score.lua').alliesScore = false
 end
