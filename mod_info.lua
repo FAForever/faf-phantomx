8efd04905f06d1c6efcb59c5adf7d87d253085e6
main <creativename1111@gmail.com>
2023-02-05 12:48:37 -0500

    fixed desc

--- mod_info.lua
+++ mod_info.lua
@@ -3,9 +3,9 @@
 -- Documentation for the extended FAF mod_info.lua format can be found here:
 -- https://github.com/FAForever/fa/wiki/mod_info.lua-documentation
 name = "Phantom-X"
-version = 264
+version = 265
 copyright = "Unknown"
-description = "Phantom-X game mod for Forged Alliance Forever. Currently does not work on the map Phantom Bunny Roanoke v12."
+description = "Phantom-X game mod for Forged Alliance Forever."
 author = "Original code by novaprim3.  Additional code by Duck_42 and mead"
 url = "https://github.com/FAForever/faf-phantomx"
 uid = "9c80a4a3-1f28-4cff-a3e7-569a7e7b2bf8"
