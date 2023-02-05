5e8cbc1f3f2d5f145474ae7db23b6f421b18b0a4
main <creativename1111@gmail.com>
2023-02-05 11:34:20 -0500

    bumped version

--- mod_info.lua
+++ mod_info.lua
@@ -3,10 +3,10 @@
 -- Documentation for the extended FAF mod_info.lua format can be found here:
 -- https://github.com/FAForever/fa/wiki/mod_info.lua-documentation
 name = "Phantom-X"
-version = 254
+version = 263
 copyright = "Unknown"
 description = "Phantom-X game mod for Forged Alliance Forever"
-author = "Original code by novaprim3.  Additional code by Duck_42 and mead."
+author = "Original code by novaprim3.  Additional code by Duck_42 and mead"
 url = "https://github.com/FAForever/faf-phantomx"
 uid = "9c80a4a3-1f28-4cff-a3e7-569a7e7b2bf8"
 selectable = true
